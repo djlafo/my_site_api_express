27a59e2ef0e5b042f3f68d3c5c54520ab255484f
Dylan Lafont <me@dylanlafont.com>
2019-03-03 01:51:12 -0600

    move config around

--- config/index.js
+++ config/index.js
@@ -1,3 +1,21 @@
+const inProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-    jwtSecret: 'test secret lol'
+    jwtSecret: inProd ? process.env.JWT_SECRET : 'test token lol',
+    production: {
+        database: process.env.DB,
+        username: process.env.DBUSER,
+        password: process.env.DBPASS,
+        host: process.env.DBHOST,
+        port: process.env.DBPORT || 5432,
+        dialect: 'postgres'
+    },
+    development: {
+        database: 'MyDatabase',
+        username: 'my_site_user',
+        password: '12345',
+        host: 'localhost',
+        port: 5432,
+        dialect: 'postgres'
+    }
 };
--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
-const inProd = process.env.NODE_ENV === 'production';
+const config = require('../config');
+const dbConfig = process.env.NODE_ENV === 'production' ? config.production : config.development;
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('MyDatabase', 'my_site_user', '12345', {
-    host: 'localhost',
-    port: 5432,
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    host: dbConfig.host,
+    port: dbConfig.port,
     dialect: 'postgres'
 });
 
