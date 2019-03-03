5bc99c0c640cf3e53f168ea52c50754b9043c788
Dylan Lafont <me@dylanlafont.com>
2019-03-03 17:38:19 -0600

    see if the error files are the problem

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 cd /home/ec2-user/api
 source /etc/environment
+[ -e forever_out.log ] && rm forever_out.log
+[ -e forever_error.log ] && rm forever_error.log
 sudo -u ec2-user forever -l forever.log -o forever_out.log -e forever_error.log start app.js
