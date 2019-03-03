90c985374e923a70c04d7e5fb1f7e7b9c4567ea0
Dylan Lafont <me@dylanlafont.com>
2019-03-03 10:41:39 -0600

    manually set the database string why not

--- migrate.sh
+++ migrate.sh
@@ -6,4 +6,4 @@ echo HOST: $DBHOST
 echo DATABASE: $DB
 echo USER: $DBUSER
 echo -------------------
-./node_modules/sequelize-cli/lib/sequelize db:migrate --env=production
+./node_modules/sequelize-cli/lib/sequelize db:migrate --env=production --url "User ID=$DBUSER;Password=$DBPASS;Host=$DBHOST;Port=$DBPORT;Database=$DB"
