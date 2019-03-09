8e4199050f59b188215d9a0ff5e70fd593860527
Dylan Lafont <me@dylanlafont.com>
2019-03-09 16:55:20 -0600

    add auth check endpoint

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const auth = require('../../auth');
 
 router.use('/posts', require('./posts'));
 router.use('/users', require('./users'));
 router.use('/files', require('./files'));
+
+router.get('/auth', auth.required, () => {});
 
 module.exports = router;
